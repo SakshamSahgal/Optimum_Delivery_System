b12669b257ece73d0140963a902f32f154c208b8
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2022-06-23 23:00:20 +0530

    Implemented Table Hover

--- Scripts/Display_Results.js
+++ Scripts/Display_Results.js
@@ -1,8 +1,34 @@
 
-function test(obj)
+function Highlight_Clusters(obj)
 {
-    alert(obj.id);
+    if(document.getElementById(obj.id).classList.contains("table_cell_highlight") == false)
+       document.getElementById(obj.id).classList.add("table_cell_highlight");
+
+    var to_highlight = (obj.id).split(","); //variable that holds current cluster to be highlighted
+    for(const i of to_highlight)
+    {
+        var id = "Node_" + i;
+        if( document.getElementById(id).classList.contains("cluster_Highlight") == false)
+           document.getElementById(id).classList.add("cluster_Highlight");
+    }
 }
+
+function Un_Highlight_Clusters(obj)
+{
+    if(document.getElementById(obj.id).classList.contains("table_cell_highlight") == true)
+        document.getElementById(obj.id).classList.remove("table_cell_highlight");
+    
+    var to_highlight = (obj.id).split(","); //variable that holds current cluster to be highlighted
+    for(const i of to_highlight)
+    {
+        var id = "Node_" + i;
+        if( document.getElementById(id).classList.contains("cluster_Highlight") == true)
+           document.getElementById(id).classList.remove("cluster_Highlight");
+    }
+}
+
+
+
 function display_clusters_generated(Calc_cluster) //display the clusters generated in the table
 {
     var table = document.getElementById("Clusters_generated_table");
@@ -24,7 +50,9 @@ function display_clusters_generated(Calc_cluster) //display the clusters generat
     {
          var this_row = table.rows[i];   
          this_row.id = (table.rows[i].cells[0].innerHTML);
-         this_row.setAttribute("onclick","test(this)"); //setting onclick event handler dynamically on each rows
+         this_row.setAttribute("onmouseover","Highlight_Clusters(this)"); //setting onclick event handler dynamically on each rows
+         this_row.setAttribute("onmouseleave","Un_Highlight_Clusters(this)"); //setting onclick event handler dynamically on each rows
+        
     }
 
 }
