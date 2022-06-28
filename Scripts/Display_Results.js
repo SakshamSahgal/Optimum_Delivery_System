feb734c165f39d79e721a175f56abb46def8b8e1
Saksham Sahgal <73836766+SakshamSahgal@users.noreply.github.com>
2022-06-28 15:32:49 +0530

    Implemented Delivery Driver Details Card

--- Scripts/Display_Results.js
+++ Scripts/Display_Results.js
@@ -1,4 +1,6 @@
 
+var cur_driver_no = -1;
+
 function Highlight_Clusters(obj) //this function is called when we hover over a cell in the table and highlights both the cluster and cell row
 {
     if(document.getElementById(obj.id).classList.contains("table_cell_highlight") == false)
@@ -50,22 +52,30 @@ function display_clusters_generated(Calc_cluster) //display the clusters generat
 
 function Highlight_path(this_path)
 {
+    document.getElementById("Driver_Details_Card").style = "";
+    var driver_no = (this_path.innerHTML).substring(11,(this_path.innerHTML).length - 5); //extracting the driver no from the table data
+    //console.log(driver_no);
+    document.getElementById("Driver_Name").innerHTML = "Driver Name = " + "Driver " + driver_no;
+    var his_cluster_table = document.getElementById("Clusters_generated_table");
+    var his_clusters = his_cluster_table.rows[driver_no].cells[0].innerHTML;
+    document.getElementById("Delivery_location").innerHTML = "Deliver_Locations = " + his_clusters;
     if(document.getElementById(this_path.id).classList.contains("table_cell_highlight") == false)
        document.getElementById(this_path.id).classList.add("table_cell_highlight");
-    console.log(this_path.id);
+    //console.log(this_path.id);
     var path = this_path.id.split(",");
     vis.show_path(path);
 }
 
 function Un_Highlight_path(this_path)
 {
-    if(document.getElementById(this_path.id).classList.contains("table_cell_highlight") == true)
-    document.getElementById(this_path.id).classList.remove("table_cell_highlight");
+   document.getElementById("Driver_Details_Card").style ="display: none;";
+   if(document.getElementById(this_path.id).classList.contains("table_cell_highlight") == true)
+   document.getElementById(this_path.id).classList.remove("table_cell_highlight");
    var path = (this_path.id).split(",");
    vis.un_highlight_path(path);
    
 }
-function display_paths(Calc_cluster) //display the clusters generated , in the table
+function display_paths(Calc_cluster) //display the paths generated , in the table
 {
     var table = document.getElementById("Drivers_path"); 
     while(table.rows.length > 1) { //clearing any previous results (if any) from the table [not deleting the first row because it is heading]
@@ -82,7 +92,6 @@ function display_paths(Calc_cluster) //display the clusters generated , in the t
              this_row.setAttribute("onmouseover","Highlight_path(this)"); //setting onclick event handler dynamically on each rows
              this_row.setAttribute("onmouseleave","Un_Highlight_path(this)"); //setting onclick event handler dynamically on each rows   
         }
-
 }
 
 function Update_Driver_Card(driver) //this function is called when we hover over a driver name
